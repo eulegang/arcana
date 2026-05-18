b17031c115d9bc32dc88b74b7a7a0214df18db4e
eulegang <eulegang@eulegang.dev>
2026-05-18 14:02:08 -0500

    Adding colors to symbols report

--- arcanac/src/reports/symbols.cpp
+++ arcanac/src/reports/symbols.cpp
@@ -1,11 +1,11 @@
 #include "../reports.h"
+#include <chroma.h>
 #include <iostream>
 
 void report::symbols(const SymbolTable &table) {
 
   for (const auto &sym : table) {
-    std::cout << sym << ":\t" << table.resolve(sym) << "\n";
+    std::cout << chroma::cyan << sym << chroma::clear << ":\t" << chroma::green
+              << table.resolve(sym) << chroma::clear << std::endl;
   }
-
-  std::cout << std::flush;
 }
