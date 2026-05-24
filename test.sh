04e0faffce02a5f5954f0095a376641d4b9e06f8
eulegang <eulegang@eulegang.dev>
2026-05-23 23:46:01 -0500

    arcana -> lang, arcanac -> compiler

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ set -e
 cmake -B build
 cmake --build build
 
-ctest --test-dir build/arcana --output-on-failure
+ctest --test-dir build/lang --output-on-failure
 ctest --test-dir build/symbol --output-on-failure
