72bc7ce666708f71b2e60c21b96b598fb14c5955
eulegang <eulegang@eulegang.dev>
2026-07-02 00:02:05 -0500

    Adding type report verbose option

--- compiler/src/reports/types.cpp
+++ compiler/src/reports/types.cpp
@@ -26,7 +26,11 @@ void types_dump_nodes(uint16_t id, sigil::Ast<arcana::Node>::Node node, void *,
 
   const arcana::types::type_id *tid = ctx->overlay.resolve(id);
   if (tid) {
-    out << " " << *tid;
+    if (!verbose) {
+      out << " " << *tid;
+    } else {
+      out << " " << ctx->base.repr(*tid);
+    }
   }
 
   out << chroma::clear << std::endl;
@@ -45,41 +49,43 @@ void report::types(const arcana::Tokens &, const arcana::Ast &ast,
       .overlay = pass.overlay,
   };
 
-  auto padd = 12;
-  for (uint16_t id = 0; id < base.bitsets.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::bs, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
-  }
+  if (!verbose) {
+    auto padd = 12;
+    for (uint16_t id = 0; id < base.bitsets.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::bs, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
 
-  for (uint16_t id = 0; id < base.enums.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::en, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
-  }
+    for (uint16_t id = 0; id < base.enums.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::en, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
 
-  for (uint16_t id = 0; id < base.structs.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::st, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
-  }
+    for (uint16_t id = 0; id < base.structs.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::st, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
 
-  for (uint16_t id = 0; id < base.derives.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::derive, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
-  }
+    for (uint16_t id = 0; id < base.derives.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::derive, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
 
-  for (uint16_t id = 0; id < base.fns.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::fn, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
-  }
+    for (uint16_t id = 0; id < base.fns.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::fn, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
 
-  for (uint16_t id = 0; id < base.aliases.size(); id++) {
-    arcana::types::type_id tid(arcana::types::type_id::cat::alias, id);
-    std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
-              << std::endl;
+    for (uint16_t id = 0; id < base.aliases.size(); id++) {
+      arcana::types::type_id tid(arcana::types::type_id::cat::alias, id);
+      std::cout << std::left << std::setw(padd) << tid << ": " << base.repr(tid)
+                << std::endl;
+    }
   }
 
   std::cout << std::endl;
